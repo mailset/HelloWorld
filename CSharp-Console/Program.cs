ab35abbfa4db96e790d406a05ccdcd98ac99a9cf
mail_set <m13703921007_1@163.com>
2022-11-16 22:25:58 +0800

    Added CSharp-Console

---
+++ CSharp-Console/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CSharp_Console_HelloWorld {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
